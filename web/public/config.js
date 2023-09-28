d65d5de8c094324874fc98c2184770ab36e7f582
Devin Jacobson <devinjacobsondj@gmail.com>
2023-09-28 18:17:14 -0400

    modified:   web/public/analyze.html 	modified:   web/public/config.js

--- web/public/config.js
+++ web/public/config.js
@@ -2,7 +2,6 @@
 var giturl = 'https://api.github.com/repos/DevinDJdj/mrrubato/commits?sha=master';
 var dbname = '/misterrubato/';
 var domain = 'https://misterrubato.com/'
-var webkey = 'AIzaSyCBCSiFLWm-3oBKvnS-yaHtU5QcFfRtZ_E';
 var channelId = 'UC4dK3RpqBq2JpIkRmQn6HOA'; //@misterrubato doesnt work in URL, probably need to escape the @, but who cares.  
 
 
