a42fab649943bd734ffe795566b98fcc6a0e7858
Devin Jacobson <devinjacobsondj@gmail.com>
2023-12-15 08:50:45 -0500

    new file:   server/ollama/RAG.py 	new file:   server/ollama/data/Orca_paper.pdf 	new file:   server/ollama/load.py 	new file:   server/ollama/test.py

---
+++ server/ollama/load.py
@@ -0,0 +1,34 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain.document_loaders import UnstructuredHTMLLoader, BSHTMLLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import GPT4AllEmbeddings
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.embeddings import FastEmbedEmbeddings #pip install fastembed
+
+#from langchain.embeddings import OllamaEmbeddings
+#dont think this is working.  
+#try to continue here 
+#https://medium.com/@vndee.huynh/build-your-own-rag-and-run-it-locally-langchain-ollama-streamlit-181d42805895
+
+import os
+
+DATA_PATH="data/"
+DB_PATH = "vectorstores/db/"
+
+def create_vector_db():
+    loader = PyPDFDirectoryLoader(DATA_PATH)
+    documents = loader.load()
+    print(f"Processed {len(documents)} pdf files")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    texts=text_splitter.split_documents(documents)
+    #this results in OSError: /lib/x86_64-linux-gnu/libc.so.6: version `GLIBC_2.32' not found
+#    vectorstore = Chroma.from_documents(documents=texts, embedding=GPT4AllEmbeddings(),persist_directory=DB_PATH)      
+#    vectorstore = Chroma.from_documents(documents=texts, embedding=LlamaCppEmbeddings(),persist_directory=DB_PATH) 
+    vectorstore = Chroma.from_documents(documents=texts, embedding=FastEmbedEmbeddings(),persist_directory=DB_PATH)      
+         
+    vectorstore.persist()
+
+if __name__=="__main__":
+    create_vector_db()
---
+++ server/ollama/test.py
@@ -0,0 +1,17 @@
+#ollama serve
+#ollama run llama2
+#python ./test.py
+#why is ./load.py not working?? changed to fastembed
+import requests
+import json
+
+url = 'http://localhost:11434/api/generate'
+
+myobj = {'model': 'llama2', 'stream': False, 'prompt': 'Why is the sky Blue?'}
+myobj = {'model': 'llama2', 'stream': False, 'prompt': 'Who wrote the document Orca: Progressive Learning from Complex Explanation Traces of GPT-4'}
+print(myobj['prompt'])
+x = requests.post(url, json = myobj)
+
+print(x.text)
+obj = json.loads(x.text)
+print(obj['response'])
