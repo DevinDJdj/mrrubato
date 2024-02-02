b755f5f9ba80d184e623b89fb340d62720919a48
Devin Jacobson <devinjacobsondj@gmail.com>
2024-02-02 14:23:52 -0500

    new file:   web/public/llm.js/182.js 	new file:   web/public/llm.js/19.js 	new file:   web/public/llm.js/218.js 	new file:   web/public/llm.js/291.js 	new file:   web/public/llm.js/363.js 	new file:   web/public/llm.js/514.js 	new file:   web/public/llm.js/521.js 	new file:   web/public/llm.js/659.js 	new file:   web/public/llm.js/864.js 	new file:   web/public/llm.js/actions.js 	new file:   web/public/llm.js/llm.js 	new file:   web/public/llm.js/models.js 	new file:   web/public/llm.js/utility.js 	new file:   web/public/llm.js/wasm/dollyv2.js 	new file:   web/public/llm.js/wasm/gpt-2.js 	new file:   web/public/llm.js/wasm/gpt-j.js 	new file:   web/public/llm.js/wasm/gpt-neox.js 	new file:   web/public/llm.js/wasm/llama.js 	new file:   web/public/llm.js/wasm/llama2.js 	new file:   web/public/llm.js/wasm/mpt.js 	new file:   web/public/llm.js/wasm/replit.js 	new file:   web/public/llm.js/wasm/starcoder.js 	new file:   web/public/llm.js/web-workers/dollyv2-worker.js 	new file:   web/public/llm.js/web-workers/gpt2-worker.js 	new file:   web/public/llm.js/web-workers/gptj-worker.js 	new file:   web/public/llm.js/web-workers/gptneox-worker.js 	new file:   web/public/llm.js/web-workers/llama-worker.js 	new file:   web/public/llm.js/web-workers/llama2-worker.js 	new file:   web/public/llm.js/web-workers/mpt-worker.js 	new file:   web/public/llm.js/web-workers/replit-worker.js 	new file:   web/public/testllm.html

---
+++ web/public/llm.js/utility.js
@@ -0,0 +1,55 @@
+const cacheName = "llm.js-cache";
+
+export async function loadBinaryResource(url, callback) {
+    let cache = null, window = self;
+
+    // Try to find if the model data is cached in Web Worker memory.
+    if (typeof window === "undefined") {
+        console.log("Oops, `window` is not defined")
+    }
+    else if(window && window.caches) {
+        cache = await window.caches.open(cacheName);
+        const cachedResponse = await cache.match(url);
+
+        if (cachedResponse) {
+            const data = await cachedResponse.arrayBuffer();
+            const byteArray = new Uint8Array(data);
+            callback(byteArray);
+            return;
+        }
+    }
+
+    const req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "arraybuffer";
+
+    req.onload = async (_) => {
+        const arrayBuffer = req.response; // Note: not req.responseText
+        if (arrayBuffer) {
+            const byteArray = new Uint8Array(arrayBuffer);
+            if (cache) await cache.put(url, new Response(arrayBuffer));
+            callback(byteArray);
+        }
+    };
+
+    req.send(null);
+}
+
+// Regex rule to remove model output from ggml scritps
+const MODEL_DEBUG_PATTERN = [
+    /main:.*/,
+    /.*_model_load:.*/
+]
+
+export const is_model_output = (text) => {
+    for(var i = 0; i < MODEL_DEBUG_PATTERN.length; i = i + 1){
+        if(MODEL_DEBUG_PATTERN[i].test(text)){
+            return false
+        }
+    }
+    return true;
+}
+
+export const extract_file_name = (text) => {
+    return text.substring(text.lastIndexOf('/')+1);
+}
