6df29d4643d45e3a651bb8f22a1424a4e10696aa
Devin Jacobson <devinjacobsondj@gmail.com>
2024-02-08 13:41:15 -0500

    new file:   backup.py

---
+++ backup.py
@@ -0,0 +1,5 @@
+#https://github.com/elesiuta/backupy
+#utilize this to generate the backups needed. 
+#pip install backupy --upgrade
+#backupy "c:\\devinpiano" "Z:\\backup\\devinpiano" --noprompt
+#backupy "c:\\devinpiano" "X:\\devinpiano" --noprompt
