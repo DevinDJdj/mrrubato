04d477b14f21ea30572e106c5a8bdc87b1d514c1
Devin Jacobson <devinjacobsondj@gmail.com>
2024-04-11 21:17:51 -0400

    deleted:    server/tss/server.py 	deleted:    server/tss/train.py 	modified:   book/20240407.txt

--- server/tss/train.py
+++
@@ -1,8 +0,0 @@
-#In here download all needed (to start just a few files)
-#along with the transcript files.  
-#Split the wav file at the transcription point.  
-#Utilize all the transcript files.  
-#If the transcript time is outside iteration, take the sample.  
-#Before analyzing if this is good enough, make sure we have human corrected data.  
-#We can keep the Times and splits coming from transcription service.  
-#We can use the samples between iterations I think ok as well.  
