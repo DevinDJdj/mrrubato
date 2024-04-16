76a32790c74a80dcf588af38f9af1683857e6cd8
Devin Jacobson <devinjacobsondj@gmail.com>
2024-04-15 22:53:01 -0400

    modified:   generate/tennis.py

--- generate/tennis.py
+++ generate/tennis.py
@@ -229,7 +229,8 @@ def get_audio_map(videoid, mediafile=None, playlist=None, args=None):
 if __name__ == '__main__':
     argparser.add_argument("--admin", help="Add Admin user", default="")
     args = argparser.parse_args()
-    get_audio_map(playlist='PLhQBpwasxUpldXpIymjy_FeQrax9qXGNT', args=args)
+#    get_audio_map(playlist='PLhQBpwasxUpldXpIymjy_FeQrax9qXGNT', args=args)
+    get_audio_map(videoid='5uFAkizQNJI', args=args) #Alcaraz Djokovic Wimbeldon 2023
 
     #next run #https://github.com/amsehili/auditok or similar to detect events.  
     #save event files to wavs and csv.  
@@ -242,4 +243,39 @@ if __name__ == '__main__':
 
     #then when detecting, we can simply use the same strategy to detect audio events.  
     #and then just categorize them with the best existing model.  
+    output_directory = "output/wavs/"
+    event_directory = "output/events/"
+    for filename in os.listdir(output_directory):
+        f = os.path.join(output_directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
+            #split the file into events.
+#git clone https://github.com/amsehili/auditok.git
+#cd auditok
+#python setup.py install            
+#sudo pip install auditok            
+            import auditok
+
+            # split returns a generator of AudioRegion objects
+            audio_regions = auditok.split(
+                f,
+                min_dur=0.2,     # minimum duration of a valid audio event in seconds
+                max_dur=4,       # maximum duration of an event
+                max_silence=0.1, # maximum duration of tolerated continuous silence within an event
+                energy_threshold=55 # threshold of detection
+            )
+
+            for i, r in enumerate(audio_regions):
+
+                # Regions returned by `split` have 'start' and 'end' metadata fields
+                print("Region {i}: {r.meta.start:.3f}s -- {r.meta.end:.3f}s".format(i=i, r=r))
+
+                # play detection
+                # r.play(progress_bar=True)
+
+                # region's metadata can also be used with the `save` method
+                # (no need to explicitly specify region's object and `format` arguments)
+                filename = r.save(event_directory + "region_{meta.start:.3f}.wav")
+                print("region saved as: {}".format(filename))
 
