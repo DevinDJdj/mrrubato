c35dc3370f32d35edd88513739ca4a254512b191
Devin Jacobson <devinjacobsondj@gmail.com>
2024-06-06 14:15:02 -0400

    modified:   book/20240509.txt 	modified:   generate/tennis2.py

--- generate/tennis2.py
+++ generate/tennis2.py
@@ -5,7 +5,11 @@ This would include a CSV of the frame number (time) and the location of the ball
 and any actions such as bounce etc.  
 This would be useful for training a further model.  
 But I really like this project as it is cohesive and understandable code and a great useful and practical example.  
+Seems it is too slow to be very practical though.  
+In order to create input for this, need to first generate the data with tennis language.  
+
 """
 #https://github.com/ArtLabss/tennis-tracking
 #this looks a bit better.  
+#too slow though at the moment.  
 
