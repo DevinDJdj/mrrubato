243a8880caf652c3df1349549e512e9558f4a5a5
Devin Jacobson <devinjacobsondj@gmail.com>
2024-08-05 00:04:00 -0400

    modified:   generate/baseball.py

--- generate/baseball.py
+++ generate/baseball.py
@@ -29,10 +29,18 @@
 #triple (-2,-6,-2)
 #home run (1,6,-6,1)
 #strike out (-9,1,-9)
-#out (-9,-6,-9)
+#ground out (-9,-6,-9)
+#fly out (-9,-2, -9)
+#line out (-9,-2,-9)
+#pop out (-9,-2,-9)
+#hit by pitch (1,7,1)
+#fielders choice (-9,-6,-9)
+#sacrifice bunt (1,2,3)
+#sacrifice fly (1,3,5)
+#foul tip (-7,-8,-7)
 #double play (-9,-11,-9)
-#foul (-7,-8,-7) (will be by far the most used sequence I guess)
-#
+#foul (-7,-10,-7) (will be by far the most used sequence I guess)
+#dirt ball (1,2,1)
 #run scored (1,-6,1)
 #sequence will be squished into the time until the next event minus 15 seconds.  
 
@@ -59,24 +67,82 @@
 #inning
 
 #position - ball strike location - base - results
-
+#no steals for now.  
 
 import pandas as pd
+import numpy as np
+import math
+import mido
+from datetime import date, timedelta
+import random
+from mido import Message, MidiFile, MidiTrack
 import requests
+from datetime import datetime
+
+
+instruments = {
+
+}
+
+previnst = [0,0,0,0,0,0,0,0,0,0]
+currentinst = [0,0,0,0,0,0,0,0,0,0]
+instcount = 0
+
+prevtime = [0,0,0,0,0,0,0,0,0,0]
+
 
 #df = pd.read_csv('c:/devinpiano/testing/data/mlb/games_wide_test.csv')
 df = pd.read_csv('c:/devinpiano/testing/data/mlb/games_wide.csv')
+
+df_schedule = pd.read_csv('c:/devinpiano/testing/data/mlb/schedules.csv')
 
 
 print(df.head())
 final_df = df.sort_values(by=['createdAt'], ascending=True)
 print(final_df.head())
-filtered_df = final_df[['homeTeamId', 'homeTeamName', 'awayTeamId', 'awayTeamName', 'venueName', 'venueCity', 'venueState', 'inningNumber', 'pitcherPitchCount', 'startingBalls', 'startingStrikes', 'startingOuts']]
+filtered_df = final_df[['gameId', 'createdAt', 'homeTeamId', 'homeTeamName', 'awayTeamId', 'awayTeamName', 'venueName', 'venueCity', 'venueState', 'inningNumber', 'pitcherPitchCount', 'startingBalls', 'startingStrikes', 'startingOuts', 'hitterId', 'awayBatter1', 'awayBatter2', 'awayBatter3', 'awayBatter4', 'awayBatter5', 'awayBatter6', 'awayBatter7', 'awayBatter8', 'awayBatter9', 'homeBatter1', 'homeBatter2', 'homeBatter3', 'homeBatter4', 'homeBatter5', 'homeBatter6', 'homeBatter7', 'homeBatter8', 'homeBatter9']]
 print(filtered_df.head())
-filtered_df2 = final_df[['createdAt', 'description', 'outcomeId', 'outcomeDescription', 'hitterId', 'hitterLastName', 'hitterFirstName', 'pitcherId', 'pitcherFirstName', 'pitcherLastName', 'pitchTypeDescription', 'pitchSpeed', 'pitchZone', 'hitLocation', 'hitType']]
+filtered_df2 = final_df[['gameId', 'createdAt', 'atBatEventType', 'description', 'outcomeId', 'outcomeDescription', 'hitterId', 'hitterLastName', 'hitterFirstName', 'pitcherId', 'pitcherFirstName', 'pitcherLastName', 'pitchTypeDescription', 'pitchSpeed', 'pitchZone', 'hitLocation', 'hitType', 'rob0_start', 'rob0_end', 'rob0_isOut', 'rob0_outcomeId', 'rob0_outcomeDescription', 'rob1_start', 'rob1_end', 'rob1_isOut', 'rob1_outcomeId', 'rob1_outcomeDescription', 'rob2_start', 'rob2_end', 'rob2_isOut', 'rob2_outcomeId', 'rob2_outcomeDescription', 'rob3_start', 'rob3_end', 'rob3_isOut', 'rob3_outcomeId', 'rob3_outcomeDescription']]
 print(filtered_df2.head())
 #homeTeamId, homeTeamName, awayTeamId, awayTeamName, venueCity, inningNumber, pitcherPitchCount, startingBalls, startingStrikes, startingOuts, 
 #createdAt, description, outcomeId, outcomeDescription, hitterId, hitterLastName, hitterFirstName, pitcherId, pitcherFirstName, pitcherLastName, pitchTypeDescription, pitchSpeed, pitchZone, hitLocation, hitType
+#awayBatter1
+
+mindate = filtered_df2.iloc[0]['createdAt']
+maxdate = filtered_df2.iloc[-1]['createdAt']
+
+for i in range(10):
+    stra = mindate[:-4]
+    prevtime[i] = datetime.strptime(stra, '%Y-%m-%d %H:%M:%S')
+
+timemap = [0]*60*24
+
+for index, row in df_schedule.iterrows():
+    datetime_str = row['startTime'][:-4]
+
+    duration = row['duration_minutes']
+    datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+    startmins = datetime_object.hour*60 + datetime_object.minute
+    for i in range(duration):
+        if (startmins + i >= 60*24):
+            startmins = startmins - 60*24
+        timemap[startmins + i] += 1
+
+print(timemap)    
+
+windowstart = 0
+windowend = 0
+for i in range(60*24):
+    if (timemap[i] == 0):
+        if (windowstart == 0):
+            windowstart = i
+    else:
+        if (windowstart !=0 and windowend == 0):
+            windowend = i
+        
+print(windowstart)
+print(windowend)
+
 
 venueMap = {'Chase Field': -112.0669444, 
             'Turner Field': -84.38944444,
@@ -113,7 +179,23 @@ venueMap = {'Chase Field': -112.0669444,
             'Nationals Park': -77.0075
             }
 
+global teamShift
 teamShift = {}
+
+
+positionMap = {'pitcher': 0, 
+               'catcher': 1,
+               'first base': 2,
+               'second base': 3,
+               'third base': 4,
+               'shortstop': 5,
+               'left field': 6,
+               'center field': 7,
+                'right field': 8,
+                'dh': 9
+                }
+
+
 
 venueMap = dict(sorted(venueMap.items(), key=lambda item: item[1]))
 print(venueMap)
@@ -126,6 +208,7 @@ for (index, row) in venueDF.iterrows():
         print(list(venueMap).index(row['venueName']))
         print(row['venueName'])
         teamShift[row['homeTeamName']] = list(venueMap).index(row['venueName'])
+
 
 print(teamShift)
 
@@ -142,22 +225,11 @@ print(venueDF.head())
 def getVelocity(row, base=31):
 
     velm = 1
-    if (row['Series'] == 'International'):
-        velm = 1
-    elif(row['Series'] == 'International Gold'):
-        velm = 1.5
-    elif(row['Series'] == 'Grand Slam'):
-        velm = 2.5
-    elif(row['Series'] == 'Masters'):
-        velm = 2
-    elif(row['Series'] == 'Masters Cup'):
-        velm = 3
-
     return int(base*velm)
 
 def getInstrument(row):
     global instcount
-    temp = row['Location'] + row['Surface']
+    temp = row['outcomeDescription']
     if (temp not in instruments.keys()):
 
         instcount = instcount + 1
@@ -165,54 +237,148 @@ def getInstrument(row):
         instruments[temp] = random.randint(0, 120)
     return instruments[temp]
 
-def getTimeFromRound(rnd, orig_time = 120): #use 120 so we are divisible by 2,3,4,5
-    mult = 1
-    if (rnd == '1st Round'):
-        mult = 1
-    elif (rnd == '2nd Round'):
-        mult = 2
-    elif (rnd == '3rd Round'):
-        mult = 4
-    elif (rnd == '4th Round'):
-        mult = 8
-    elif (rnd == 'Quarterfinals'):
-        mult = 16
-    elif (rnd == 'Semifinals'):
-        mult = 32
-    elif (rnd == 'The Final'):
-        mult = 64
-    return mult*orig_time
+def getTime(row, track, orig_time = 120): #use 120 so we are divisible by 2,3,4,5
+    global windowstart, windowend
+    mytime = datetime.strptime(row['createdAt'], '%Y-%m-%d %H:%M:%S UTC')
+
+    numseconds = 0
+    if (mytime.hour*60 + mytime.minute > windowend and prevtime[track].hour*60 + prevtime[track].minute < windowstart):
+        #new day add time until 
+        #add time between prevdate and windowstart + mytime - windowend
+        numseconds = windowstart*60 - ((prevtime[track].hour*60 + prevtime[track].minute)*60 + prevtime[track].second) + mytime.hour*60 + mytime.minute +mytime.second - windowend*60
+        #add time for this interval
+    else:
+        numseconds = mytime.hour*60*60 + mytime.minute*60+mytime.second - prevtime[track].hour*60*60 - prevtime[track].minute*60 - prevtime[track].second
+        #possible there is millisecond hits etc.  
+    prevtime[track] = mytime
+
+    return numseconds
+
+def getShift(row, away=False):
+    if (away):
+        shift = teamShift[row['awayTeamName']]
+    else:
+        shift = teamShift[row['homeTeamName']]
+    return shift
 
 
-def getShift(row):
-    shift = abs(row['WRank'] - row['LRank'])
-    sign = 1
-    if (row['LRank'] > row['WRank']):
-        sign = -1
-    if (np.isnan(shift)):
-        shift = 1
-    elif(shift > math.pow(math.e, 6)):
-        shift = math.pow(math.e, 6)
 
-    shift = math.log(shift)
-    shift = shift/2
-    return shift*sign
 
-def getDates(start=2005, end=2006):
-    alldates = []
-    start_date = date(start, 1, 1)
-    end_date = date(end, 1, 1)
-    delta = timedelta(days=1)
-    while start_date <= end_date:
-        alldates.append(start_date.strftime("%Y-%m-%d"))
-#        print(start_date.strftime("%Y-%m-%d"))
-        start_date += delta
-    return alldates
+def getBatterId(row):
+    hitterid = row['hitterId']
+    for i in range(1, 10):
+        if (row['homeBatter' + str(i)] == hitterid):
+            return i, False
+        elif (row['awayBatter' + str(i)] == hitterid):
+            return i, True
+    print(row)
+    return 0, False
+
+def getNotesFromEvent(row, baserow):
+    if (row['outcomeDescription'] == 'Ball' and baserow['startingBalls'] == 3):
+        return [1,5,1]
+    elif (row['outcomeDescription'] == 'Hit by pitch'):
+        return [1,7,1]
+    elif (row['outcomeDescription'] == 'Strike Looking' and baserow['startingStrikes'] == 2):
+        return []
+    elif (row['outcomeDescription'] == 'Strike Swinging' and baserow['startingStrikes'] == 2):
+        return []
+    elif (row['outcomeDescription'] == 'Foul'):
+        return [-7,-10,-7]
+    elif (row['outcomeDescription'] == 'Single'):
+        return [1,6,1]
+    elif (row['outcomeDescription'] == 'Dirt Ball'):
+        return [1,2,1]
+    elif (row['outcomeDescription'] == 'Double'):
+        return [6,-2,6]
+    elif (row['outcomeDescription'] == 'Triple'):
+        return [-2,-6,-2]
+    elif (row['outcomeDescription'] == 'Homerun'):
+        return [1,6,-6,1]
+    elif (row['outcomeDescription'] == 'Foul Tip' and baserow['startingStrikes'] == 2):
+        return [-9,1,-9]
+    elif (row['outcomeDescription'] == 'Foul Tip'):
+        return [-7,-8,-7]
+    elif (row['outcomeDescription'] == 'Strikeout'):
+        return [-9,1,-9]
+    elif (row['outcomeDescription'] == 'Ground Out'):
+        return [-9,-6,-9]
+    elif (row['outcomeDescription'] == 'Fielders Choice'):
+        return [-9,-6,-9]
+    elif (row['outcomeDescription'] == 'Fly Out'):
+        return [-9,-2,-9]
+    elif (row['outcomeDescription'] == 'Line Out'):
+        return [-9,-2,-9]
+    elif (row['outcomeDescription'] == 'Pop Out'):
+        return [-9,-2,-9]
+    elif (row['outcomeDescription'] == 'Double Play'):
+        return [-9,-11,-9] 
+    elif (row['outcomeDescription'] == 'Sacrifice Bunt'):
+        return [1,2,3]
+    elif (row['outcomeDescription'] == 'Sacrifice Fly'):
+        return [1,3,5]
+    elif (row['outcomeDescription'] == 'Run Scored'):
+        return [1,-6,1]   
+    elif (row['outcomeDescription'] == 'Double Play'):
+        return [-9,-11,-9]
+
+    return []
+
+def getNotesFromRuns(row):
+    ret = []
+    for i in range(0, 4):
+        start = row['rob' + str(i) + '_start']
+        if (start is not None and start != ""):
+            start = i
+            end = row['rob' + str(i) + '_end']
+            if (end == 4):
+                ret += [1,-6,1]
+            elif (end == 3):
+                ret +=[-6,-4,-6]
+            elif (end == 2):
+                ret +=[-2,0,-2]
+            elif (end == 1):
+                ret +=[6,8,6]
+
+    return ret
+#first (6,8,6)
+#second (-2,0,-2)
+#third (-6,-4,-6)
+
+
+
+    
+
+def addNotes(row, baserow, trackid, numseconds, myvel, shift, mid):
+    basenote = 60 + shift
+    events = getNotesFromEvent(row, baserow)
+    runs = getNotesFromRuns(row)
+    track = mid.tracks[trackid]
+    totalmessageseconds = 120
+    messageseconds = math.floor(totalmessageseconds/(len(events)+len(runs)+4))
+    #message length 120 seconds
+    #playing just before the event actually takes place.  
+    if (numseconds < 120*2):
+        #problem here.  
+        messageseconds = math.floor(numseconds/(len(events)+len(runs)+4))
+        track.append(Message('note_on', note=basenote, velocity=myvel, time=messageseconds*2))
+        track.append(Message('note_off', note=basenote, velocity=myvel, time=messageseconds*2))
+    else:
+        track.append(Message('note_on', note=basenote, velocity=1, time=numseconds-totalmessageseconds))
+        track.append(Message('note_off', note=basenote, velocity=127, time=messageseconds*4))
+    #this is actually the time between previous and now.  We want this to be static.  But what is the average time between?  
+    for i in events:
+        track.append(Message('note_on', note=basenote + i, velocity=myvel, time=messageseconds))
+        track.append(Message('note_off', note=basenote + i, velocity=127, time=messageseconds))
+    for i in runs:
+        track.append(Message('note_on', note=basenote + i, velocity=myvel, time=messageseconds))
+        track.append(Message('note_off', note=basenote + i, velocity=127, time=messageseconds))
+                     
 
 mid = MidiFile()
 
 programs = []
-numtracks = 4
+numtracks = 10 #number of positions.  
 for n in range(numtracks):
     track = MidiTrack()
     programs.append(1)
@@ -227,69 +393,46 @@ for n in range(numtracks):
 #track.append(Message('note_off', note=65, velocity=127, time=32))
 
 
-df = pd.read_csv('C:/devinpiano/testing/tennis/data/archive/atp_mens_tour/2005.csv')
-print(df)
+trackcnt = 32
+#each team has a track for now.  
+#perhaps want to do something with the position of the player with hit sound.  
 
-sorted_df = df.sort_values(by=['Date'], ascending=True)
+for index, row in filtered_df.iterrows():
+    gameId = row['gameId']
+    createdAt = row['createdAt']
+    row2 = filtered_df2[(filtered_df2['gameId'] == gameId) & (filtered_df2['createdAt'] == createdAt)]
+#    print(row2)
+    row2 = row2.iloc[0]
+    if (row2['atBatEventType'] == 'STEAL'):
+        continue
+    if (row2['atBatEventType'] == 'PITCH'):
 
-trackcnt = 0
+        #trackid = batterid for now
+        batterid, away = getBatterId(row)
+        if (batterid ==0):
+            continue
+        trackid = batterid
+        #team key shift 0 - 32, guess 16 is 60.  
+        shift = getShift(row, away)
 
-alldates = getDates(2005, 2006)
 
-for d in alldates:
-    mask = df['Date'] == d
-    if (len(df[mask]) > 0):
-        mylen = len(df[mask])
-
-        if (trackcnt < numtracks-1):
-            trackcnt = trackcnt + 1
-        else:
-            trackcnt = 0
+        trackcnt = batterid
         
-        for index, row in df[mask].iterrows():
-            if (not (np.isnan(row['Wsets'] or np.isnan(row['Lsets'])))):
-                shift = getShift(row)
+        track = mid.tracks[trackid]
+
+        numseconds = getTime(row2, trackid) #how do we sync up the time, use time since last event.  
+
+        myvel = getVelocity(row2)
 
 
-                totalsets = int(row['Wsets'] + row['Lsets'])
-                currentinst[trackcnt] = getInstrument(row)
-                if (currentinst[trackcnt] != previnst[trackcnt]):
-                    track.append(Message('program_change', program=currentinst[trackcnt], time=0))
-                    previnst[trackcnt] = currentinst[trackcnt]
-                    print('new instrument ' + str(currentinst[trackcnt]))
-                    print(instruments)
-                mytime = getTimeFromRound(row['Round'])
-#                trackcnt = int(math.log2(mytime/120))
-#                print(mytime)
-                track = mid.tracks[trackcnt]
-                if (totalsets > 0):
-                    mytime = int(mytime/totalsets) #each round should be same time.  
-                myvel = getVelocity(row)
-                for x in range(1, totalsets+1):
-
-            #        print(row['W' + str(x)])
-            #        print(row['L' + str(x)])
-
-                    #get base note based on WRank/LRank
-                    basenote = 60 + int(shift*12)
-
-                    basenote = 60 + (int(shift*2)%7)*6
-
-
-                    track.append(Message('note_on', note=basenote + int(row['W' + str(x)]), velocity=myvel, time=mytime))
-                    track.append(Message('note_off', note=basenote + int(row['W' + str(x)]), velocity=127, time=mytime))
-                    track.append(Message('note_on', note=basenote + int(row['L' + str(x)]), velocity=myvel, time=mytime))
-                    track.append(Message('note_off', note=basenote + int(row['L' + str(x)]), velocity=127, time=mytime))
+        addNotes(row2, row, trackid, numseconds, myvel, shift, mid)
+        if index%10000 == 0:
+            print(str(index) + " records complete")
+            print(row2)
 
     else:
         #delay for average time if day off.  
         myvel = 0
         basenote = 60
-        mytime = getTimeFromRound('')
-        mytime *= len(df)/365
-        mytime = int(mytime)
-        track.append(Message('note_on', note=basenote, velocity=myvel, time=mytime))
-        track.append(Message('note_off', note=basenote, velocity=myvel, time=mytime))
-        track.append(Message('note_on', note=basenote, velocity=myvel, time=mytime))
-        track.append(Message('note_off', note=basenote, velocity=myvel, time=mytime))
 
+mid.save('baseball.mid')
