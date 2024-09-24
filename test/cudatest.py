2bde5bd951967b4d583d0ce8138a50e7b3058a86
devinjacobsondj@gmail.com <devinjacobsondj@gmail.com>
2024-09-23 23:43:08 -0400

    modified:   .gitignore 	new file:   book/20240902.txt 	new file:   server/tts/mozilla/config.json 	new file:   test/cudatest.py

---
+++ test/cudatest.py
@@ -0,0 +1,28 @@
+import torch
+import tensorflow as tf
+import cv2
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+print("Torch:")
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(0))
+
+print("CV2:")
+if cv2.cuda.getCudaEnabledDeviceCount() > 0:
+    print('Yes')
+else:
+    print('No')
+print(cv2.cuda.printCudaDeviceInfo(0))
+
+print("Tensorflow:")
+print(tf.__version__)
+print(tf.config.list_physical_devices())
+print(tf.test.is_gpu_available())
+print(tf.test.is_built_with_cuda())
+print(tf.sysconfig.get_build_info()['cuda_version'])
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
