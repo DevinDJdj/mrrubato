bcb5fe9e6b27494c76bea7a977be9130edbe5468
devinjacobsondj@gmail.com <devinjacobsondj@gmail.com>
2024-12-05 21:43:45 -0500

    modified:   book/20241204.txt 	modified:   news.py

--- news.py
+++ news.py
@@ -1,5 +1,19 @@
 #https://github.com/codelucas/newspaper
 #pip3 install newspaper3k
+#too many runs will make the newspaper block.  
+#create UI to manage paper/input sources.  
+# then have aggregation settings.  
+#and feedback loop.  
+#not simple like/dislike though.  Need much more nuanced feedback.  
+#why not just provide human language feedback.  
+#"I would like to read about ...."
+#"What happened in Tampa today?"
+#"Find some heartwarming story for me.."
+# Then provide feedback
+#"Give me the headlines"
+#"Read that story"
+# Need decent TTS for this.  
+
 
 import newspaper
 
@@ -17,11 +31,11 @@ cnn_article.download()
 cnn_article.parse()
 cnn_article.nlp()
 
-print(article.keywords)
+print(cnn_article.keywords)
 
-print(article.summary)
+print(cnn_article.summary)
 
-print(article.text)
+print(cnn_article.text)
 
-print(article.authors)
-print(article.publish_date)
+print(cnn_article.authors)
+print(cnn_article.publish_date)
