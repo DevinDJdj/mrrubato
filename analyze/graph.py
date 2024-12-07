b155a682e5d8365d858c37ac7dfbfc866fcd6c6a
devinjacobsondj@gmail.com <devinjacobsondj@gmail.com>
2024-12-06 23:09:03 -0500

    new file:   analyze/graph.py 	modified:   book/20240429.txt 	modified:   book/20241204.txt

---
+++ analyze/graph.py
@@ -0,0 +1,15 @@
+#https://github.com/kerighan/kinbaku
+
+#https://github.com/graphistry/pygraphistry
+
+#use these libraries to create a graph of relationship between sequence iterations.  
+#Node for each NGRAM.  
+#Only analyze sets which have meaning across a certain timespan.  
+#what timespan is meaningful?  
+#time segmentation only may be somewhat interesting.  
+#Lets say timespan of between 2 and 20 seconds to start.  
+#what are the graph bonds between two iterations and same iteration etc.  
+#between other pieces
+#analyze/analyze.py
+#getNGrams()
+
