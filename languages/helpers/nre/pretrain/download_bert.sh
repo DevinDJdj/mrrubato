82b7396832240c5fc696e660e73fbd987e0a4193
devinjacobsondj@gmail.com <devinjacobsondj@gmail.com>
2026-08-16 16:53:45 -0400

    deleted:    languages/helpers/nre/.gitignore 	deleted:    languages/helpers/nre/LICENSE 	deleted:    languages/helpers/nre/README.md 	deleted:    languages/helpers/nre/benchmark/download_fewrel.sh 	deleted:    languages/helpers/nre/benchmark/download_nyt10.sh 	deleted:    languages/helpers/nre/benchmark/download_nyt10m.sh 	deleted:    languages/helpers/nre/benchmark/download_semeval.sh 	deleted:    languages/helpers/nre/benchmark/download_wiki20m.sh 	deleted:    languages/helpers/nre/benchmark/download_wiki80.sh 	deleted:    languages/helpers/nre/benchmark/rel4wiki80.md 	deleted:    languages/helpers/nre/example/test_multilabel_bert.py 	deleted:    languages/helpers/nre/example/test_multilabel_cnn.py 	deleted:    languages/helpers/nre/example/train_bag_bert.py 	deleted:    languages/helpers/nre/example/train_bag_cnn.py 	deleted:    languages/helpers/nre/example/train_supervised_bert.py 	deleted:    languages/helpers/nre/example/train_supervised_cnn.py 	deleted:    languages/helpers/nre/opennre/__init__.py 	deleted:    languages/helpers/nre/opennre/encoder/__init__.py 	deleted:    languages/helpers/nre/opennre/encoder/base_encoder.py 	deleted:    languages/helpers/nre/opennre/encoder/bert_encoder.py 	deleted:    languages/helpers/nre/opennre/encoder/cnn_encoder.py 	deleted:    languages/helpers/nre/opennre/encoder/pcnn_encoder.py 	deleted:    languages/helpers/nre/opennre/framework/__init__.py 	deleted:    languages/helpers/nre/opennre/framework/bag_re.py 	deleted:    languages/helpers/nre/opennre/framework/data_loader.py 	deleted:    languages/helpers/nre/opennre/framework/multi_label_sentence_re.py 	deleted:    languages/helpers/nre/opennre/framework/sentence_re.py 	deleted:    languages/helpers/nre/opennre/framework/utils.py 	deleted:    languages/helpers/nre/opennre/model/__init__.py 	deleted:    languages/helpers/nre/opennre/model/bag_attention.py 	deleted:    languages/helpers/nre/opennre/model/bag_average.py 	deleted:    languages/helpers/nre/opennre/model/bag_one.py 	deleted:    languages/helpers/nre/opennre/model/base_model.py 	deleted:    languages/helpers/nre/opennre/model/sigmoid_nn.py 	deleted:    languages/helpers/nre/opennre/model/softmax_nn.py 	deleted:    languages/helpers/nre/opennre/module/__init__.py 	deleted:    languages/helpers/nre/opennre/module/nn/__init__.py 	deleted:    languages/helpers/nre/opennre/module/nn/cnn.py 	deleted:    languages/helpers/nre/opennre/module/nn/lstm.py 	deleted:    languages/helpers/nre/opennre/module/nn/rnn.py 	deleted:    languages/helpers/nre/opennre/module/pool/__init__.py 	deleted:    languages/helpers/nre/opennre/module/pool/avg_pool.py 	deleted:    languages/helpers/nre/opennre/module/pool/max_pool.py 	deleted:    languages/helpers/nre/opennre/pretrain.py 	deleted:    languages/helpers/nre/opennre/tokenization/__init__.py 	deleted:    languages/helpers/nre/opennre/tokenization/basic_tokenizer.py 	deleted:    languages/helpers/nre/opennre/tokenization/bert_tokenizer.py 	deleted:    languages/helpers/nre/opennre/tokenization/utils.py 	deleted:    languages/helpers/nre/opennre/tokenization/word_piece_tokenizer.py 	deleted:    languages/helpers/nre/opennre/tokenization/word_tokenizer.py 	deleted:    languages/helpers/nre/pretrain/download_bert.sh 	deleted:    languages/helpers/nre/pretrain/download_glove.sh 	deleted:    languages/helpers/nre/requirements.txt 	deleted:    languages/helpers/nre/setup.py 	deleted:    languages/helpers/nre/tests/test_inference.py

--- languages/helpers/nre/pretrain/download_bert.sh
+++
@@ -1,4 +0,0 @@
-mkdir bert-base-uncased
-wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/config.json
-wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/pytorch_model.bin
-wget -P bert-base-uncased https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/bert-base-uncased/vocab.txt
--- languages/helpers/nre/pretrain/download_glove.sh
+++
@@ -1,3 +0,0 @@
-mkdir glove
-wget -P glove https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_mat.npy
-wget -P glove https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_word2id.json
